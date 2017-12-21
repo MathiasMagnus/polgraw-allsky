f143f204e3ca6ff6480ca27e1c1de9996ba7832a
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2017-12-21 12:51:54 +0100

    Modified file paths to match rebase

--- search/network/src-opencl/inc/locations.in.h
+++ search/network/src-opencl/inc/locations.in.h
@@ -1,2 +1,2 @@
-const char* kernel_path = "${CMAKE_CURRENT_SOURCE_DIR}/kernels.cl";
-const char* kernel_inc_path = "${CMAKE_CURRENT_SOURCE_DIR}";
+const char* kernel_path = "${CMAKE_CURRENT_SOURCE_DIR}/krn/kernels.cl";
+const char* kernel_inc_path = "${CMAKE_CURRENT_SOURCE_DIR}/krn";
