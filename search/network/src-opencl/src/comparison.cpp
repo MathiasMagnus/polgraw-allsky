56c5b0b83717f06ad8340c5ad7f55fec20ddb0c4
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2019-05-30 13:54:22 +0200

    fixed 'explicit specialization in non-namespace scope'

--- search/network/src-opencl/src/comparison.cpp
+++ search/network/src-opencl/src/comparison.cpp
@@ -19,21 +19,28 @@ enum member
     signal_to_noise
 };
 
+struct signal;
+
+namespace detail
+{
+    template <member M> bool compare_signals(const signal& lhs, const signal& rhs);
+}
+
 struct signal
 {
     double frequency,
-           spindown,
-           declination,
-           ascension,
-           signal_to_noise;
+        spindown,
+        declination,
+        ascension,
+        signal_to_noise;
 
     bool operator==(const signal& other) const
     {
         return frequency == other.frequency &&
-               spindown == other.spindown &&
-               declination == other.declination &&
-               ascension == other.ascension &&
-               signal_to_noise == other.signal_to_noise;
+            spindown == other.spindown &&
+            declination == other.declination &&
+            ascension == other.ascension &&
+            signal_to_noise == other.signal_to_noise;
     };
 
     bool operator<(const signal& other) const
@@ -67,13 +74,10 @@ struct signal
     }
 
     template <member M>
-    static bool compare(const signal& lhs, const signal& rhs);
-
-    template <> static bool compare<member::frequency>(const signal& lhs, const signal& rhs) { return lhs.frequency < rhs.frequency; }
-    template <> static bool compare<member::spindown>(const signal& lhs, const signal& rhs) { return lhs.spindown < rhs.spindown; }
-    template <> static bool compare<member::declination>(const signal& lhs, const signal& rhs) { return lhs.declination < rhs.declination; }
-    template <> static bool compare<member::ascension>(const signal& lhs, const signal& rhs) { return lhs.ascension < rhs.ascension; }
-    template <> static bool compare<member::signal_to_noise>(const signal& lhs, const signal& rhs) { return lhs.signal_to_noise < rhs.signal_to_noise; }
+    static bool compare(const signal& lhs, const signal& rhs)
+    {
+        return detail::compare_signals<M>(lhs, rhs);
+    }
 };
 
 std::istream& operator>>(std::istream& is, signal& sig)
@@ -103,24 +107,30 @@ std::ostream& operator<<(std::ostream& os, const signal& sig)
     return os;
 }
 
+template <> static bool detail::compare_signals<member::frequency>(const signal& lhs, const signal& rhs) { return lhs.frequency < rhs.frequency; }
+template <> static bool detail::compare_signals<member::spindown>(const signal& lhs, const signal& rhs) { return lhs.spindown < rhs.spindown; }
+template <> static bool detail::compare_signals<member::declination>(const signal& lhs, const signal& rhs) { return lhs.declination < rhs.declination; }
+template <> static bool detail::compare_signals<member::ascension>(const signal& lhs, const signal& rhs) { return lhs.ascension < rhs.ascension; }
+template <> static bool detail::compare_signals<member::signal_to_noise>(const signal& lhs, const signal& rhs) { return lhs.signal_to_noise < rhs.signal_to_noise; }
+
 /// <summary>
 ///     For every element in range [first1, last1) copies the nearest element from the range [first2, last2)
 ///     to d_first, according to the user provided norm.
 /// </summary>
 template <typename InputIt1, typename RandomIt2, typename OutputIt, typename Norm>
 OutputIt copy_nearest(InputIt1 first1, InputIt1 last1,
-                      RandomIt2 first2, RandomIt2 last2,
-                      OutputIt d_first,
-                      Norm norm)
+    RandomIt2 first2, RandomIt2 last2,
+    OutputIt d_first,
+    Norm norm)
 {
     while (first1 != last1)
     {
         *d_first++ =
             *std::min_element(first2, last2,
-                                       [=](const auto& a, const auto& b)
-            {
-                return norm(*first1, a) < norm(*first1, b);
-            });
+                [=](const auto& a, const auto& b)
+                {
+                    return norm(*first1, a) < norm(*first1, b);
+                });
         first1++;
     }
     return d_first;
@@ -130,32 +140,32 @@ int main(int argc, char* argv[])
 {
     // Test
     std::string ocl_path{ argv[1] },
-                ref_path{ argv[2] },
-                out_path{ argv[3] };
+        ref_path{ argv[2] },
+        out_path{ argv[3] };
 
     std::ifstream ocl_stream{ ocl_path, std::ios::in },
-                  ref_stream{ ref_path, std::ios::in };
+        ref_stream{ ref_path, std::ios::in };
 
     if (!ocl_stream.is_open() || !ref_stream.is_open())
         return -1;
 
     std::vector<signal> ocl_vec(std::istream_iterator<signal>{ ocl_stream },
-                                std::istream_iterator<signal>{}),
-                        ref_vec(std::istream_iterator<signal>{ ref_stream },
-                                std::istream_iterator<signal>{});
+        std::istream_iterator<signal>{}),
+        ref_vec(std::istream_iterator<signal>{ ref_stream },
+            std::istream_iterator<signal>{});
 
     auto norm =
         [freq_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<frequency>)->frequency - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<frequency>)->frequency,
-         spin_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<spindown>)->spindown - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<spindown>)->spindown,
-         decl_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<declination>)->declination - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<declination>)->declination,
-         asc_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<ascension>)->ascension - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<ascension>)->ascension,
-         snr_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<signal_to_noise>)->signal_to_noise - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<signal_to_noise>)->signal_to_noise](const signal& x, const signal& y)
+        spin_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<spindown>)->spindown - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<spindown>)->spindown,
+        decl_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<declination>)->declination - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<declination>)->declination,
+        asc_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<ascension>)->ascension - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<ascension>)->ascension,
+        snr_range = std::max_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<signal_to_noise>)->signal_to_noise - std::min_element(ref_vec.cbegin(), ref_vec.cend(), signal::compare<signal_to_noise>)->signal_to_noise](const signal& x, const signal& y)
     {
         return std::abs(x.frequency - y.frequency) / freq_range +
-               std::abs(x.spindown - y.spindown) / spin_range +
-               std::abs(x.declination - y.declination) / decl_range +
-               std::abs(x.ascension - y.ascension) / asc_range +
-               std::abs(x.signal_to_noise - y.signal_to_noise) / snr_range;
+            std::abs(x.spindown - y.spindown) / spin_range +
+            std::abs(x.declination - y.declination) / decl_range +
+            std::abs(x.ascension - y.ascension) / asc_range +
+            std::abs(x.signal_to_noise - y.signal_to_noise) / snr_range;
     };
 
     std::cout << ocl_path << " has " << ocl_vec.size() << " data points." << std::endl;
@@ -163,15 +173,15 @@ int main(int argc, char* argv[])
 
     std::vector<signal> filtered;
     copy_nearest(ref_vec.cbegin(), ref_vec.cend(),
-                 ocl_vec.cbegin(), ocl_vec.cend(),
-                 std::back_inserter(filtered),
-                 norm);
+        ocl_vec.cbegin(), ocl_vec.cend(),
+        std::back_inserter(filtered),
+        norm);
 
     std::ofstream out_full{ out_path + ".full.bin" };
     out_full << std::scientific;
     out_full.precision(6);
     std::copy(filtered.cbegin(), filtered.cend(),
-              std::ostream_iterator<signal>{ out_full });
+        std::ostream_iterator<signal>{ out_full });
     /*
     std::sort(ref_vec.begin(), ref_vec.end());
     std::sort(filtered.begin(), filtered.end());
@@ -195,13 +205,13 @@ int main(int argc, char* argv[])
     */
     std::vector<double> diff;
     std::transform(ref_vec.cbegin(), ref_vec.cend(), filtered.cbegin(), std::back_inserter(diff), norm);
-    
+
     auto avg_diff = std::accumulate(diff.cbegin(), diff.cend(), 0.0) / diff.size();
     auto dev_diff = std::sqrt(std::accumulate(diff.cbegin(), diff.cend(), 0.0,
-                                              [=](const double& acc, const double& val)
-                                              {
-                                                  return acc + std::pow(val - avg_diff, 2);
-                                              }) / (diff.size() - 1));
+        [=](const double& acc, const double& val)
+        {
+            return acc + std::pow(val - avg_diff, 2);
+        }) / (diff.size() - 1));
 
     std::cout << "Avarage difference: " << avg_diff << std::endl;
     std::cout << "Standard deviation of differences: " << dev_diff << std::endl;
